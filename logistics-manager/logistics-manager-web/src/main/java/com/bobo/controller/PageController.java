d6057a74f5df040e27a29df3a9c28df3cca2e323
long <875374086@qq.com>
2024-04-02 10:15:05 +0800

    项目框架搭建完毕，静态资源引入完毕，可以开始具体jsp页面以及增删改查功能的编写了

---
+++ logistics-manager/logistics-manager-web/src/main/java/com/bobo/controller/PageController.java
@@ -0,0 +1,28 @@
+package com.bobo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    /**
+     * 跳转到主页面的servlet
+     * @return
+     */
+    @RequestMapping(value = {"/","/home"})
+    public String showMain(){
+        return "main";
+    }
+
+    /**
+     * 跳转到对应的页面
+     * @return
+     */
+    @RequestMapping(value = {"/{path}"})
+    public String showPage(@PathVariable String path){
+        return path;
+    }
+
+}
